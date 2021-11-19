9bc5c596e9474e53398bb4ab9bb568484b1bfebc
Maxousorus <60946689+Maxousorus@users.noreply.github.com>
2021-11-19 10:40:21 +0100

    Add comment at start of code

--- MiniProjetJava.java
+++ MiniProjetJava.java
@@ -1,3 +1,8 @@
+/*
+MiniProjetJava | Création d'un programme de mini-jeu de mémoire.
+BOUDIER Maxime, BAYEN Maxime, CHAUNEY Célian
+ */
+
 public class MiniProjetJava {
 
     public static void main(String[] args){
