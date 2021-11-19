cc0744b3b12c95fbea1659aa23ab708579a603d4
Maxousorus <60946689+Maxousorus@users.noreply.github.com>
2021-11-19 20:43:30 +0100

    Read text from Extrait-texte.txt

--- MiniProjetJava.java
+++ MiniProjetJava.java
@@ -3,9 +3,23 @@ MiniProjetJava | Création d'un programme de mini-jeu de mémoire.
 BOUDIER Maxime, BAYEN Maxime, CHAUNEY Célian
  */
 
+import java.io.IOException;
+
 public class MiniProjetJava {
 
     public static void main(String[] args){
+
+        String text;
+        try {
+            text = Lecteur.LireTexte("src/Extrait_texte.txt");
+        }
+        catch (IOException ex){
+            System.out.println("Erreur de la lecture du fichier.");
+            text = "ERROR";
+        }
+
+        System.out.println(text);
+
 
     }
 }
