63b5a8950b153e9ebac394e17939aad65ae0817d
Maxousorus <60946689+Maxousorus@users.noreply.github.com>
2021-11-19 20:48:29 +0100

    Method getText to get text from .txt

--- MiniProjetJava.java
+++ MiniProjetJava.java
@@ -9,17 +9,22 @@ public class MiniProjetJava {
 
     public static void main(String[] args){
 
+        String text = getText("src/Extrait_texte.txt");
+
+        System.out.println(text);
+    }
+
+    static String getText(String filepath){
+
         String text;
         try {
-            text = Lecteur.LireTexte("src/Extrait_texte.txt");
+            text = Lecteur.LireTexte(filepath);
         }
         catch (IOException ex){
             System.out.println("Erreur de la lecture du fichier.");
             text = "ERROR";
         }
 
-        System.out.println(text);
-
-
+        return text;
     }
 }
